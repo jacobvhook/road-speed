9d7d92f36949afb460329cbc15eb9d957d44d96c
Pedro Lemos <lemos.pj@gmail.com>
2024-04-11 10:22:34 -0400

    Speed hump data augmentation (#24)

--- data-exploration/data_downloader.py
+++ data-exploration/data_downloader.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import shape
+from shapely import wkt
 from sodapy import Socrata
 
 
@@ -10,22 +11,46 @@ class OpenDataDownloader:
     def __init__(self, app_token: str):
         self.app_token = app_token
 
-    def load_data(self, dataset: str, limit: int | None = None) -> pd.DataFrame:
+    def load_data(
+        self,
+        dataset: str | None = None,
+        limit: int | None = None,
+        save_to_file: str | None = None,
+        load_from_file: str | None = None,
+    ) -> pd.DataFrame:
+        if load_from_file is not None:
+            return pd.read_csv(load_from_file, low_memory=False)
         client = Socrata("data.cityofnewyork.us", app_token=self.app_token)
         results = client.get(dataset, limit=limit)
-        return pd.DataFrame.from_records(results)
+        df = pd.DataFrame.from_records(results)
+        if save_to_file is not None:
+            df.to_csv(save_to_file, index=False)
+        return df
 
     def load_geo_dataframe(
         self,
-        dataset: str,
-        geometry_column: str,
+        dataset: str | None = None,
+        geometry_column: str | None = None,
         crs: str = "EPSG:4326",
         to_crs: str | None = None,
         limit: int | None = None,
+        load_from_file: str | None = None,
+        save_to_file: str | None = None,
     ) -> gpd.GeoDataFrame:
+        if load_from_file is not None:
+            df = pd.read_csv(load_from_file, low_memory=False)
+            df["geometry"] = df[geometry_column].apply(wkt.loads)
+            if geometry_column != "geometry":
+                df.drop(columns=geometry_column, inplace=True)
+            return gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs)
         df = self.load_data(dataset, limit)
         df["geometry"] = df[geometry_column].apply(shape)
-        df.drop(columns=[geometry_column], inplace=True)
+        if geometry_column != "geometry":
+            df.drop(columns=[geometry_column], inplace=True)
         if to_crs is None:
-            return gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs)
-        return gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs).to_crs(to_crs)
+            gdf = gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs)
+        else:
+            gdf = gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs).to_crs(to_crs)
+        if save_to_file is not None:
+            gdf.to_csv(save_to_file, index=False)
+        return gdf
