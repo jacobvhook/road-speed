73646e54f1278025ecbc0139d76e1d2dc607e491
Jesse Frohlich <jesse@frohli.ch>
2024-04-12 09:16:52 -0400

    Replace serialization format; clean up `OpenDataDownloader`'s methods.
    This is part-way through separating data-cleaning from data-downloading.

--- src/data_downloader.py
+++ src/data_downloader.py
@@ -23,20 +23,23 @@ class OpenDataDownloader:
         limit: int = 3_000_000,
         force_download: bool = False,
     ) -> pd.DataFrame:
-        data_path: Path = Path(f"../data/{dataset}.csv")
+        data_path: Path = Path(f"../data/{dataset}.pkl")
         if data_path.exists() and not force_download:
-            return pd.read_csv(data_path, low_memory=False)
+            return pd.read_pickle(data_path)
         client = Socrata("data.cityofnewyork.us", app_token=self.app_token)
         results = client.get(ENDPOINTS[dataset], limit=limit)
         df = pd.DataFrame.from_records(results)
-        df.to_csv(data_path, index=False)
+        df.to_pickle(data_path)
         return df
 
-    def to_geo_dataframe(self, df: pd.DataFrame, geometry_column: str, crs: str):
-        df["geometry"] = df[geometry_column].apply(wkt.loads)
-        if geometry_column != "geometry":
-            df.drop(columns=geometry_column, inplace=True)
-        return gpd.GeoDataFrame(df, geometry=df.geometry, crs=crs)
+    def from_geometry_column(
+        self, df: pd.DataFrame, geometry_column: str, crs: str = geo.STD_EPSG
+    ):
+        """Convert a DataFrame to a GeoDataFrame using NYC coordinates."""
+        df.rename(columns={geometry_column: "geometry"}, inplace=True)
+        return gpd.GeoDataFrame(df, geometry=df.geometry.apply(shape), crs=crs).to_crs(
+            geo.NYC_EPSG
+        )
 
     def load_geo_dataframe(
         self,
@@ -48,6 +51,13 @@ class OpenDataDownloader:
         load_from_file: str | None = None,
         save_to_file: str | None = None,
     ) -> gpd.GeoDataFrame:
+        return self.to_geo_dataframe(
+            self.load_data(
+                dataset,
+            )
+        )
+        # load_data(dataset).to_geo_...()
+
         if load_from_file is not None:
             df = pd.read_csv(load_from_file, low_memory=False)
             df["geometry"] = df[geometry_column].apply(wkt.loads)
