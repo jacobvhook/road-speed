06b478210839a4de9caee126d4d4582aa938ec63
Pedro Lemos <lemos.pj@gmail.com>
2024-04-17 12:18:05 -0400

    Refactor `data_helpers.py`
    - Refactors the class in `data-helpers.py` so that it can be used in the
    aggregation of road features other than collisions.
    - Updates `weight_assignment.ipynb` in the `examples` folder.

--- src/data_helpers.py
+++ src/data_helpers.py
@@ -6,18 +6,20 @@ from pathlib import Path
 from numpy import datetime64, timedelta64
 
 
-class CollisionRatesCalculator:
+class RoadFeaturesCalculator:
 
-    def __init__(self, crashes_df: gpd.GeoDataFrame, streets_df: gpd.GeoDataFrame):
-        self.crashes = crashes_df
+    def __init__(self, features_df: gpd.GeoDataFrame, streets_df: gpd.GeoDataFrame):
+        self.features = features_df
         self.streets = streets_df
 
-    def __get_week_span(self) -> float:
-        crash_dates = self.crashes["crash_date"].apply(datetime64)
+    def __get_week_span(self, date_column: str) -> float:
+        crash_dates = self.features[date_column].apply(datetime64)
         return (crash_dates.max() - crash_dates.min()) / timedelta64(1, "W")
 
     def __get_intersection_weights(
-        self, intersection_data_path: Path = Path("../data/intersection_weights.pkl")
+        self,
+        buffer: int = 30,
+        intersection_data_path: Path = Path("../data/intersection_weights.pkl"),
     ) -> gpd.GeoDataFrame:
         if intersection_data_path.exists():
             intersections = pd.read_pickle(intersection_data_path)
@@ -25,7 +27,7 @@ class CollisionRatesCalculator:
                 intersections, geometry=intersections.geometry, crs=geo.NYC_EPSG
             )
         intersections = gpd.GeoDataFrame(
-            geometry=self.streets["geometry"].boundary.buffer(30), crs=geo.NYC_EPSG
+            geometry=self.streets["geometry"].boundary.buffer(buffer), crs=geo.NYC_EPSG
         )
 
         # Dissolve and explode to get the connected components
@@ -48,38 +50,43 @@ class CollisionRatesCalculator:
         intersections.to_pickle(intersection_data_path)
         return intersections
 
-    def calculate_collision_rates(
+    def calculate_road_features(
         self,
+        id_column: str,
+        feature_name: str,
+        date_column: str | None = None,
         method: str = "weighted",
+        buffer: int = 30,
         intersection_data_path: Path = Path("../data/intersection_weights.pkl"),
     ) -> pd.DataFrame:
         if method == "weighted":
             intersection_weights = self.__get_intersection_weights(
-                intersection_data_path
+                buffer=buffer, intersection_data_path=intersection_data_path
             )
-            weighted_crashes = self.crashes.sjoin(
+            weighted_features = self.features.sjoin(
                 intersection_weights, how="left", predicate="within"
-            )[["collision_id", "geometry", "weight"]]
+            )[[id_column, "geometry", "weight"]]
         elif method == "uniform":
-            weighted_crashes = self.crashes[["collision_id", "geometry"]].copy()
-            weighted_crashes["weight"] = 1.0
+            weighted_features = self.features[[id_column, "geometry"]].copy()
+            weighted_features["weight"] = 1.0
         else:
             raise NotImplementedError(f"Method {method} has not been implemented.")
-        weighted_crashes["weight"] = weighted_crashes["weight"].fillna(value=1.0)
+        weighted_features["weight"] = weighted_features["weight"].fillna(value=1.0)
 
         buffered_streets = self.streets.copy()
-        buffered_streets["geometry"] = buffered_streets["geometry"].buffer(30)
+        buffered_streets["geometry"] = buffered_streets["geometry"].buffer(buffer)
 
-        collision_rates = (
-            buffered_streets.sjoin(weighted_crashes, how="left")
+        feature_aggregation = (
+            buffered_streets.sjoin(weighted_features, how="left")
             # TODO: decide what columns we want to keep.
             # Perhaps we want to pass them as arguments?
             .groupby(by="physicalid", as_index=False)["weight"]
             .sum()
-            .rename(columns={"weight": "collision_rate"})
+            .rename(columns={"weight": feature_name})
         )
-        week_span = self.__get_week_span()
-        collision_rates["collision_rate"] = (
-            collision_rates["collision_rate"] / week_span
-        )
-        return collision_rates
+        if date_column is not None:
+            week_span = self.__get_week_span(date_column)
+            feature_aggregation[feature_name] = (
+                feature_aggregation[feature_name] / week_span
+            )
+        return feature_aggregation
