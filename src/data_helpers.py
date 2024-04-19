6a9853f24df38760a13d3bfe6ec8e38b7c8e83e1
Jesse Frohlich <jesse@frohli.ch>
2024-04-19 17:35:38 -0400

    Implement `calculate_linear_road_features`.
    While exclusively for the use of speedhumps, this function is able to
    take linear feature data and connect them to the streets `DataFrame`.

--- src/data_helpers.py
+++ src/data_helpers.py
@@ -1,16 +1,20 @@
-import geopandas as gpd
-import pandas as pd
-from shapely import is_empty
-import geo
+from datetime import datetime
 from pathlib import Path
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
 from numpy import datetime64, timedelta64
+from shapely import is_empty
+
+import geo
 
 
 class RoadFeaturesCalculator:
 
-    def __init__(self, features_df: gpd.GeoDataFrame, streets_df: gpd.GeoDataFrame):
-        self.features = features_df
-        self.streets = streets_df
+    def __init__(self, features: gpd.GeoDataFrame, streets: gpd.GeoDataFrame):
+        self.features = features
+        self.streets = streets
 
     def __get_week_span(self, date_column: str) -> float:
         crash_dates = self.features[date_column].apply(datetime64)
@@ -50,12 +54,42 @@ class RoadFeaturesCalculator:
         intersections.to_pickle(intersection_data_path)
         return intersections
 
-    def calculate_road_features(
+    def calculate_linear_road_features(
         self,
-        id_column: str,
-        feature_name: str,
+        output_column: str,
+        install_date_column: str | None = None,
+        predicate: str = "contains",
+        buffer: float = 30,
+    ):
+        self.features[install_date_column] = self.features[install_date_column].apply(
+            datetime64
+        )
+        if predicate == "contains":
+            buffered_features = self.features.copy()
+            buffered_features["geometry"] = buffered_features["geometry"].buffer(buffer)
+            features_aggregate = (
+                buffered_features.sjoin(self.streets, how="right", predicate=predicate)
+                .groupby(by="physicalid", as_index=False)[install_date_column]
+                .max()
+                .rename(columns={install_date_column: "after"})
+            )
+        else:
+            raise NotImplementedError(
+                f"Predicate {predicate} is not implemented for calculate_linear_road_features."
+            )
+        features_aggregate[output_column] = ~features_aggregate["after"].isna()
+        features_aggregate["until"] = np.nan
+        features_old = features_aggregate[features_aggregate[output_column]].copy()
+        features_old[output_column] = False
+        features_old.rename(columns={"after": "until", "until": "after"}, inplace=True)
+        return pd.concat([features_aggregate, features_old])
+
+    def calculate_point_road_features(
+        self,
+        aggregate_column: str,
         date_column: str | None = None,
-        method: str = "weighted",
+        installation_date_column: str | None = None,
+        method: str | None = None,
         buffer: int = 30,
         intersection_data_path: Path = Path("../data/intersection_weights.pkl"),
     ) -> pd.DataFrame:
@@ -65,28 +99,52 @@ class RoadFeaturesCalculator:
             )
             weighted_features = self.features.sjoin(
                 intersection_weights, how="left", predicate="within"
-            )[[id_column, "geometry", "weight"]]
+            )[["geometry", "weight"]].fillna(value=1.0)
         elif method == "uniform":
-            weighted_features = self.features[[id_column, "geometry"]].copy()
+            weighted_features = self.features[["geometry"]].copy()
             weighted_features["weight"] = 1.0
         else:
             raise NotImplementedError(f"Method {method} has not been implemented.")
-        weighted_features["weight"] = weighted_features["weight"].fillna(value=1.0)
 
         buffered_streets = self.streets.copy()
         buffered_streets["geometry"] = buffered_streets["geometry"].buffer(buffer)
 
         feature_aggregation = (
-            buffered_streets.sjoin(weighted_features, how="left")
+            weighted_features.sjoin(buffered_streets, how="right", predicate="within")
             # TODO: decide what columns we want to keep.
             # Perhaps we want to pass them as arguments?
             .groupby(by="physicalid", as_index=False)["weight"]
             .sum()
-            .rename(columns={"weight": feature_name})
+            .rename(columns={"weight": aggregate_column})
         )
         if date_column is not None:
             week_span = self.__get_week_span(date_column)
-            feature_aggregation[feature_name] = (
-                feature_aggregation[feature_name] / week_span
+            feature_aggregation[aggregate_column] = (
+                feature_aggregation[aggregate_column] / week_span
             )
         return feature_aggregation
+
+
+class FeatureJoiner:
+
+    def __init__(self, features: gpd.GeoDataFrame, streets: gpd.GeoDataFrame):
+        self.features = features
+        self.streets = streets
+
+    def add_data_with_installation_date(
+        self,
+        date_column: str,
+    ):
+        self.features[date_column] = self.features[date_column].apply(datetime64)
+        # self.streets.pass()
+
+    def add_data_with_time_data(
+        self,
+    ):
+        pass
+
+    def add_data(self):
+        self.streets = self.streets.join(self.features, on="physicalid", how="left")
+
+
+# centerline <- speedhumps <- crashes, trees, ...
