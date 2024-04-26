d414d479aa39fab0d716fe40bee8d031810e59ea
Pedro Lemos <lemos.pj@gmail.com>
2024-04-26 13:39:56 -0400

    Model comparison (#34)
    * Incorporates KNN model into `examples/regression_traffic_vol.ipynb`
    
    * Updates `README.md` and incorporates the baseline model in `examples/regression_traffic_vol.ipynb`
    
    * Creates a model comparison bar chart
    
    * Adds model paths
    
    * Adds some text to `examples/test.ipynb`

--- src/model_helpers/model_paths.py
+++ src/model_helpers/model_paths.py
@@ -1,5 +1,7 @@
 MODEL_PATHS = {
+    "baseline": "../models/regression/baseline.pkl",
     "linear": "../models/regression/base_linear.pkl",
+    "knn": "../models/regression/knn.pkl",
     "random_forest": "../models/regression/random_forest.pkl",
     "xgboost": "../models/regression/xgboost.pkl",
 }
